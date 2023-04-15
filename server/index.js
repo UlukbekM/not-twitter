cc5409e265e5f256f5a13651dd040904fcc47273
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2023-04-14 22:11:21 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const jwt = require("jsonwebtoken");
 // https://www.section.io/engineering-education/how-to-build-authentication-api-with-jwt-token-in-nodejs/
 
